2160bbc5dda2851fa26fed27995ef4a6935101c9
KitsuneSemCalda <arthuramagalhaes1312@gmail.com>
2022-08-31 20:32:15 -0300

    first commit contain the struct of Medicamentos

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+struct Medicamentos{
+    string NomeMedicamento;
+    string Descricao;
+    string Laboratorio;
+    string CodigoIndentificacao;
+    int QuantidadeDisponivel;
+    float PrecoUnitario;
+    bool SituacaoProduto;
+};
