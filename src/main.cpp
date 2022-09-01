9f5a97314d97bb8750358578db3ec64070035b21
KitsuneSemCalda <arthuramagalhaes1312@gmail.com>
2022-09-01 18:04:59 -0300

    feat: Improved source code

--- src/main.cpp
+++ src/main.cpp
@@ -3,100 +3,118 @@
 
 using namespace std;
 
-struct Medicamentos{
-    string NomeMedicamento;
-    string Descricao;
-    string Laboratorio;
-    string CodigoIndentificacao;
-    int QuantidadeDisponivel;
-    float PrecoUnitario;
-    bool SituacaoProduto;
+struct Medicamentos {
+  string NomeMedicamento;
+  string Descricao;
+  string Laboratorio;
+  string CodigoIndentificacao;
+  int QuantidadeDisponivel;
+  float PrecoUnitario;
+  bool SituacaoProduto;
 };
 
 /*
-Funcao Vazia Cadastro que gera os valores para serem cadastrados no registro Medicamentos 
+Funcao Vazia Cadastro que gera os valores para serem cadastrados no registro
+Medicamentos
 */
-void Cadastro(){
-    // Gerando um ponteiro cujo tamanho inicial comece com 3 para criar o vetor de registro
-    int *TamVetor;
-    TamVetor = new int;
-    *TamVetor = 3;
+void Cadastro() {
+  // Gerando um ponteiro cujo tamanho inicial comece com 3 para criar o vetor de
+  // registro
+  int *TamVetor;
+  TamVetor = new int;
+  *TamVetor = 3;
 
-    //Gerando um registro do tipo Medicamentos para o vetor
-    Medicamentos VecMed[*TamVetor];
+  // Gerando um registro do tipo Medicamentos para o vetor
+  Medicamentos VecMed[*TamVetor];
 
-    for (int i = 0; i != *TamVetor; i++){
-        cout << "------------------------------------------------" << endl;
-        cout << "Digite o nome do Produto: ";
-        
-        string NomeMed;
-        getline(cin >> ws,NomeMed);
-        VecMed[i].NomeMedicamento = NomeMed;
+  for (int i = 0; i != *TamVetor; i++) {
+    cout << "------------------------------------------------" << endl;
+    cout << "Digite o nome do Produto: ";
 
-        cout << "Digite uma descrição do Produto: ";
-        string Description;
-        getline(cin >> ws,Description);
-        VecMed[i].Descricao = Description;
-        
-        cout << "Digite o Laborátorio que criou o produto: ";
-        string Labor;
-        getline(cin >> ws,Labor);
-        VecMed[i].Laboratorio = Labor;
-        
-        cout << "Digite o código de identificação do produto: ";
-        string CodInd;
-        getline(cin >> ws, CodInd);
-        VecMed[i].CodigoIndentificacao = CodInd;
-        
-        cout << "Digite o tamanho do estoque deste produto: ";
-        int EstoqueDisponivel;
-        cin >> EstoqueDisponivel;
-        VecMed[i].QuantidadeDisponivel = EstoqueDisponivel;
-        
-        cout << "Digite o preço Unitário do produto: ";
-        float PrecoUnitario;
-        cin >> PrecoUnitario;
-        VecMed[i].PrecoUnitario = PrecoUnitario;
-        
-        cout << "Digite o status do produto: ";
-        string StatusProduto;
-        cin >> StatusProduto;
-        if (StatusProduto == "Ativo"){
-            VecMed[i].SituacaoProduto = true;
-        }else if (StatusProduto == "Inativo"){
-            VecMed[i].SituacaoProduto = false;
-        }
-        cout << "Cadastro Efetuado com Sucesso!\n";
+    string NomeMed;
+    getline(cin >> ws, NomeMed);
+    VecMed[i].NomeMedicamento = NomeMed;
+
+    cout << "Digite uma descrição do Produto: ";
+    string Description;
+    getline(cin >> ws, Description);
+    VecMed[i].Descricao = Description;
+
+    cout << "Digite o Laborátorio que criou o produto: ";
+    string Labor;
+    getline(cin >> ws, Labor);
+    VecMed[i].Laboratorio = Labor;
+
+    cout << "Digite o código de identificação do produto: ";
+    string CodInd;
+    getline(cin >> ws, CodInd);
+    VecMed[i].CodigoIndentificacao = CodInd;
+
+    cout << "Digite o tamanho do estoque deste produto: ";
+    int EstoqueDisponivel;
+    cin >> EstoqueDisponivel;
+    VecMed[i].QuantidadeDisponivel = EstoqueDisponivel;
+
+    cout << "Digite o preço Unitário do produto: ";
+    float PrecoUnitario;
+    cin >> PrecoUnitario;
+    VecMed[i].PrecoUnitario = PrecoUnitario;
+
+    cout << "Digite o status do produto: ";
+    string StatusProduto;
+    cin >> StatusProduto;
+    if (StatusProduto == "Ativo") {
+      VecMed[i].SituacaoProduto = true;
+    } else if (StatusProduto == "Inativo") {
+      VecMed[i].SituacaoProduto = false;
     }
-    /*
-    for (int i = 0; i != *TamVetor; i++){
-        cout << VecMed[i].NomeMedicamento << endl << VecMed[i].Descricao << endl;
-        cout << VecMed[i].Laboratorio << endl << VecMed[i].CodigoIndentificacao << endl;
-        cout << VecMed[i].QuantidadeDisponivel << endl << VecMed[i].SituacaoProduto << endl;
+    cout << "Cadastro Efetuado com Sucesso!\n";
+  }
+
+  cout << "------------------------------------------------------------------"
+       << endl;
+
+  for (int i = 0; i != *TamVetor; i++) {
+    cout << "Nome do Medicamento: " << VecMed[i].NomeMedicamento << endl;
+    cout << "Descrição do Medicamento: " << VecMed[i].Descricao << endl;
+    cout << "Laboratorio em que o medicamento foi criado: "
+         << VecMed[i].Laboratorio << endl;
+    cout << "Codigo de Identificação do medicamento: "
+         << VecMed[i].CodigoIndentificacao << endl;
+    cout << "Quantidade Disponivel em estoque: "
+         << VecMed[i].QuantidadeDisponivel << endl;
+    if (VecMed[i].SituacaoProduto == true) {
+      cout << "Situação do Produto: Ativo" << endl;
+    } else {
+      cout << "Situação do Produto: Inativo" << endl;
     }
-    */
+    cout << "---------------------------------------------------------------"
+         << endl;
+  }
 }
 /*
     Adicionando uma função simples de menu
 */
-void menu(){
-    int Option = 0;
-        while (Option != 8){
-        cout << "------------------------------------------------" << endl;
-        cout << "Lista de Opções:\n\t1 - Cadastrar Produto\n\t2 - Consultar Medicamento\n\t3 - Listar Medicamento\n";
-        cout << "\t4 - Excluir Medicamento\n\t5 - Efetuar uma Venda\n\t6 - Listar dados de medicamentos em estoque\n\t";
-        cout << "7 - Exportar Dados para arquivos\n\t8 - Sair\n";
-        cout << "Digite a opção que você deseja realizar: ";
-        cin >> Option;
-        switch (Option){
-            case 1:
-                Cadastro();
-                break;
-        }
+void menu() {
+  int Option = 0;
+  while (Option != 8) {
+    cout << "------------------------------------------------" << endl;
+    cout << "Lista de Opções:\n\t1 - Cadastrar Produto\n\t2 - Consultar "
+            "Medicamento\n\t3 - Listar Medicamento\n";
+    cout << "\t4 - Excluir Medicamento\n\t5 - Efetuar uma Venda\n\t6 - Listar "
+            "dados de medicamentos em estoque\n\t";
+    cout << "7 - Exportar Dados para arquivos\n\t8 - Sair\n";
+    cout << "Digite a opção que você deseja realizar: ";
+    cin >> Option;
+    switch (Option) {
+    case 1:
+      Cadastro();
+      break;
     }
+  }
 }
 
-int main(){
-    menu();
-    return 0;
+int main() {
+  menu();
+  return 0;
 }
